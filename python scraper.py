4b6b79b88e311aeb5fe6595c9bcb45dc047e111c
Steve <claudefrancois@gmail.com>
2025-02-07 12:08:58 +0100

    Changé le nom du fichier main en python scraper.py

--- python scraper.py
+++ python scraper.py
@@ -1,4 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
+url = "https://books.toscrape.com"
 
+response = requests.get(url)
+
+print(response.status_code) # Code 200 = tout va bien !
+if response.status_code == 200:
+    print("C'est bon, j'ai eu un retour du site !")
+
+# print(response.headers) # affiche les headers de booktoscrape
+print(response.text) # le contenu en texte
