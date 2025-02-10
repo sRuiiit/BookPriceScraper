e0e3a742a4cc81b55449a8a515d43899664c2d44
Steve <claudefrancois@gmail.com>
2025-02-10 12:47:51 +0100

    extraction du champs et embellisement du code

--- scraper.py
+++ scraper.py
@@ -1,27 +1,37 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
+# URL de la page à scraper
 url = "https://books.toscrape.com/catalogue/rip-it-up-and-start-again_986/index.html"
 
-# Vérification que la page URL est disponible avec requests
+# Effectuer une requête GET pour récupérer la page
 response = requests.get(url)
 
-print(response.status_code) # Code 200 = tout va bien !
 if response.status_code == 200:
-    print("C'est bon, j'ai eu un retour du site !\n")
+    print("C'est bon, j'ai eu un retour du site !")
 
-# Analyse du contenu HTML avec BeautifulSoup
-soup = BeautifulSoup(response.content, "html.parser")
+    # Analyse du contenu HTML avec BeautifulSoup
+    soup = BeautifulSoup(response.content, "html.parser")
 
-# Chercher la div avec la classe "col-sm-6 product_main"
-div_product_main = soup.find("div", class_="col-sm-6 product_main")
+    # Définition des champs extraits et leurs sélecteurs CSS correspondants
+    champs_extraits = {
+        "div.col-sm-6.product_main > h1": soup.find("div", class_="col-sm-6 product_main").find("h1").get_text(),
+        # "p.price_color": soup.find("p", class_="price_color").get_text(), <-- si je veux le champs couleur
+        # "p.instock.availability": soup.find("p", class_="instock availability").get_text(strip=True) <-- si je veux le champs dispo
+    }
 
-# Chercher le h1 à l'intérieur de cette div
-titre_page_body = div_product_main.find("h1").get_text()
+    # Nom du fichier CSV
+    nom_fichier = "donnees_extraites.csv"
 
-print("Titre de la page :", titre_page_body)
+    # Création et écriture du fichier CSV
+    with open(nom_fichier, mode="w") as file:
+        writer = csv.writer(file)
 
+        # Écriture de l'en-tête avec les sources des données (titres de colonnes)
+        writer.writerow(champs_extraits.keys())
 
+        # Écriture des valeurs extraites
+        writer.writerow(champs_extraits.values())
 
-
-
+    print(f"Le fichier {nom_fichier} a été créé avec succès !")
