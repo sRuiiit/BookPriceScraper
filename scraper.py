2f216427ed91043e80a6b40c945e9d51e17b0c48
Steve <claudefrancois@gmail.com>
2025-02-10 12:19:39 +0100

    Changé le nom du fichier main en python scraper.py

--- scraper.py
+++ scraper.py
@@ -3,16 +3,20 @@ from bs4 import BeautifulSoup
 
 url = "https://books.toscrape.com/catalogue/rip-it-up-and-start-again_986/index.html"
 
+# Vérification que la page URL est disponible
 response = requests.get(url)
 
 print(response.status_code) # Code 200 = tout va bien !
 if response.status_code == 200:
-    print("C'est bon, j'ai eu un retour du site !")
+    print("C'est bon, j'ai eu un retour du site !\n")
 
 # Analyse du contenu HTML avec BeautifulSoup
 soup = BeautifulSoup(response.content, "html.parser")
 
-# Extraction du titre de la page
-titre_page = soup.title.string
-print("Titre de la page :", titre_page)
+# Extraction du titre de la page (dans head)
+# titre_page = soup.title.string
+# print("Titre de la page :", titre_page[0:30])
 
+# Ou aller chercher le titre dans (body)
+titre_page_body = soup.find("h1").get_text()
+print("Titre de la page :", titre_page_body)s
